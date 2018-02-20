377882032c9a11b9a7f571140a3247b2e6eece96
flatsiedatsie <unfold@gmail.com>
2018-02-21 00:45:39 +0100

    Added fully automatic mode
    With the new 'automatic' mode, the plugin does a bluetooth scan and enables all MiFlora devices it can find.

--- plugin.py
+++ plugin.py
@@ -4,7 +4,13 @@
 		This plugin connects to Mi Flower Mate flower sensors over Bluetooth LE. It requires the BluePy library to be installed on the system.
 	</description>
 	<params>
-		<param field="Mode1" label="Devices mac adresses, capitalised and comma separated" width="300px" required="true" default="C4:7C:8D:62:50:34"/>
+	    <param field="Mode1" label="Device selection" width="300px" required="true">
+            <options>
+                <option label="Automatic scanning" value="auto" default="true"/>
+                <option label="Manual selection (add below)" value="manual"/>
+            </options>
+        </param>
+		<param field="Mode2" label="Devices mac adresses, capitalised and comma separated" width="300px" required="false" default=""/>
 	</params>
 </plugin>
 """
@@ -17,6 +23,10 @@ except ImportError:
 	import fakeDomoticz as Domoticz
 import time
 import sys
+import shelve
+import os
+from miflora import miflora_scanner, BluepyBackend
+import miflora
 try:	
 	from miflora.miflora_poller import MiFloraPoller, \
     	MI_CONDUCTIVITY, MI_MOISTURE, MI_LIGHT, MI_TEMPERATURE, MI_BATTERY
@@ -29,38 +39,72 @@ except:
 
 class BasePlugin:
 
-	def __init__(self):
-		#Domoticz.Debugging(1)   
+	def __init__(self): 
+		self.macs = []
 		return 
 
 	def onStart(self):
+		Domoticz.Debugging(1)
+		
 		sys.path.append("/usr/local/lib/python3.4/dist-packages")
 		sys.path.append("/usr/local/lib/python3.5/dist-packages")
 	   
 		if bluepyError == 1:
-			Domoticz.Log("error loading Flora libraries")
+			Domoticz.Error("Error loading Flora libraries")
 		
-		self.domoticzServer=Parameters["Address"]
-		self.port=Parameters["Port"]
-		self.webPath=str(self.domoticzServer) + ":" + str(self.port)
-		self.macs = parseCSV(Parameters["Mode1"])
 		
-		Domoticz.Debug("macs = {}".format(self.macs))
 
-		Domoticz.Log("Mi Flora - Macs = " + str(Parameters["Mode1"]))
-		Domoticz.Log("Mi Flora - devices made so far (max 255): " + str(len(Devices)))
+		Domoticz.Debug("Mi Flora - devices made so far (max 255): " + str(len(Devices)))
 		
-		# create the listen-toggle-switch. This controls the making of new RF switches.
+		# create master toggle switch
 		if 1 not in Devices:
 			Domoticz.Log("Creating the master Mi Flower Mate poll switch. Flip it to poll the sensors.")
 			Domoticz.Device(Name="Flip to update Mi Flowermates", Unit=1, TypeName="Switch", Image=9, Used=1).Create()
 		
+		# get the mac addresses of the sensors
+		if Parameters["Mode1"] == 'auto':
+			Domoticz.Log("Automatic mode is selected")
+			self.floraScan()
+		else:
+			Domoticz.Log("Manual mode is selected")
+			self.macs = parseCSV(Parameters["Mode2"])
+			self.createSensors()
+		#Domoticz.Log("macs = {}".format(self.macs))
+	
+
+	def onStop(self):
+		Domoticz.Log("onStop called")
+
+	def onConnect(self, Connection, Status, Description):
+		Domoticz.Log("onConnect called")
+
+	def onMessage(self, Connection, Data, Status, Extra):
+		Domoticz.Log("onMessage called")
+
+	def onCommand(self, Unit, Command, Level, Hue):
+		Domoticz.Log("onCommand called for Unit " + str(Unit) + ": Parameter '" + str(Command) + "', Level: " + str(Level))		
 		
+		# flip the switch icon, and then get the plant data.
+		if Unit == 1:
+			if str(Command) == "On":
+				Devices[Unit].Update(nValue=1,sValue="On")
+			if str(Command) == "Off":
+				Devices[Unit].Update(nValue=0,sValue="Off")
+			self.getPlantData()
+		  
+	
+	def onHeartbeat(self):
+		pass
+	
+	
+	# function to create corresponding sensors in Domoticz if there are Mi Flower Mates which don't have them yet.
+	def createSensors(self):
+		# create the domoticz sensors if necessary
 		if ((len(Devices) - 1)/4) < len(self.macs):
-			Domoticz.Log("Creating new sensors")
+			Domoticz.Debug("Creating new sensors")
 			# Create the sensors. Later we get the data.
 			for idx, mac in enumerate(self.macs):
-				Domoticz.Log("Creating devices for mac: "+str(mac))
+				Domoticz.Debug("Creating new sensors for Mi Flower Mate at "+str(mac))
 				sensorBaseName = "#" + str(idx) + " "
 
 				#moisture
@@ -69,8 +113,8 @@ class BasePlugin:
 				if sensorNumber not in Devices:
 				
 					sensorName = sensorBaseName + "Moisture"
-					Domoticz.Log("Creating first sensor, #"+str(sensorNumber))
-					Domoticz.Log("Creating first sensor, name: "+str(sensorName))
+					Domoticz.Debug("Creating first sensor, #"+str(sensorNumber))
+					Domoticz.Debug("Creating first sensor, name: "+str(sensorName))
 					Domoticz.Device(Name=sensorName, Unit=sensorNumber, TypeName="Percentage", Used=1).Create()   
 					Domoticz.Log("Created device: "+Devices[sensorNumber].Name)
 
@@ -93,42 +137,16 @@ class BasePlugin:
 					sensorNumber = (idx*4) + 5
 					sensorName = sensorBaseName + "Conductivity"
 					Domoticz.Device(Name=sensorName, Unit=sensorNumber, TypeName="Custom", Used=1).Create()
-					Domoticz.Log("Created device: "+Devices[sensorNumber].Name)			
+					Domoticz.Log("Created device: "+Devices[sensorNumber].Name)
 
 
-	def onStop(self):
-		Domoticz.Log("onStop called")
-
-	def onConnect(self, Connection, Status, Description):
-		Domoticz.Log("onConnect called")
-
-	def onMessage(self, Connection, Data, Status, Extra):
-		Domoticz.Log("onMessage called")
-
-	def onCommand(self, Unit, Command, Level, Hue):
-		Domoticz.Log("onCommand called for Unit " + str(Unit) + ": Parameter '" + str(Command) + "', Level: " + str(Level))		
-		
-		# flip the switch icon, and then get the plant data.
-
-		if Unit == 1:
-			if str(Command) == "On":
-				Devices[Unit].Update(nValue=1,sValue="On")
-			if str(Command) == "Off":
-				Devices[Unit].Update(nValue=0,sValue="Off")
-			  
-			self.getPlantData()
-		  
-	
-	def onHeartbeat(self):
-		pass
-
-	
+	# function to poll a Flower Mate for its data
 	def getPlantData(self):
 		for idx, mac in enumerate(self.macs):
 			Domoticz.Log("getting data from sensor: "+str(mac))
 		 
 			poller = MiFloraPoller(str(mac), BluepyBackend)
-			Domoticz.Log("Firmware: {}".format(poller.firmware_version()))
+			Domoticz.Debug("Firmware: {}".format(poller.firmware_version()))
 			
 			val_bat  = int("{}".format(poller.parameter_value(MI_BATTERY)))
 			nValue = 0
@@ -164,7 +182,49 @@ class BasePlugin:
 			# give bluetooth a little breathing room
 			time.sleep(1)
 
+	# function to scan for devices, and store and compare the outcome
+	def floraScan(self):
+		Domoticz.Log("Scanning for Mi Flower Mate sensors..")
+				
+		#databaseFile=os.path.join(os.environ['HOME'],'XiaomiMiFlowerMates')
+		# first, let's get the list of devices we already know about
+		database = shelve.open('XiaomiMiMates')
+		
+		try:
+			knownSensors = database['macs']
+			oldLength = len(knownSensors)
+			Domoticz.Debug("Already know something:" + str(oldLength))
+			Domoticz.Log("Already known devices:" + str(knownSensors))
+		except:
+			knownSensors = []
+			database['macs'] = knownSensors
+			oldLength = 0;
+			Domoticz.Debug("No existing sensors in system?")
+		
+		#Next we scan to look for new sensors
+		try:
+			foundFloras = miflora_scanner.scan(BluepyBackend, 5)
+			Domoticz.Log("Number of devices found via bluetooth scan = " + str(len(foundFloras)))
+		except:
+			foundFloras = []
+			Domoticz.Log("Scan failed")
+			
+		for sensor in foundFloras:
+			if sensor not in knownSensors:
+				knownSensors.append(str(sensor))
+				Domoticz.Log("Found new device: " + str(sensor))
+				
+		if len(knownSensors) != oldLength:
+			database['macs'] = knownSensors
+			Domoticz.Log("Updating database")
+		
+		database.close()	
 	
+		self.macs = knownSensors
+		self.createSensors()
+		#return knownSensors
+			
+
 	
 global _plugin
 _plugin = BasePlugin()
@@ -186,12 +246,9 @@ def onHeartbeat():
 	_plugin.onHeartbeat()
 	
 
-
-
 def parseCSV(strCSV):
 	listvals = []
 	for value in strCSV.split(","):
 		listvals.append(value)
 	return listvals
-
 
